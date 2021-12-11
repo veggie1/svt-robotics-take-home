01c59e8274929f9612e373c63b03f6b948dcf54b
Ben Baacke <ben.baacke@gmail.com>
2021-12-11 10:39:16 -0500

    Replace WeatherController with RobotController

---
+++ SVTRoboticsTakeHome/SVTRoboticsTakeHome/Controllers/RobotController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SVTRoboticsTakeHome.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RobotController : ControllerBase
+    {
+        // GET: api/<RobotController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST api/<RobotController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
